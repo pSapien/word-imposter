4da07f675861d78570c60f7885bd4d5d0b4164d2
psapien <prateekthapa26@gmail.com>
2025-08-13 10:51:32 +0545

    refactor: update endpoint and remove debug logs

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const Constants = {
     SelectedRole: "$$selectedRole$$",
     GameSettings: "$$game_settings$$",
   },
-  Endpoint: "https://a0217d0b33f1.ngrok-free.app",
+  Endpoint: "https://667e09f9f4d7.ngrok-free.app",
   PingInterval: 1000 * 5,
   ShowDebugLogs: true,
 };
